309f098fab3b053f500998360d909692fc9f311b
AlessandroGervasi <a.gervasi4@campus.unimib.it>
2025-02-06 12:05:01 +0100

    Aggiunta navigazione tra detailsDrink

--- src/views/DrinkDetails/DrinkDetails.jsx
+++ src/views/DrinkDetails/DrinkDetails.jsx
@@ -1,14 +1,14 @@
-import {useParams} from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
-import style from "./DrinkDetails.module.css"
-import simbolino1 from "../../assets/images/simbolino1.svg"
-import simbolino2 from "../../assets/images/simbolino2.svg"
+import style from "./DrinkDetails.module.css";
+import simbolino1 from "../../assets/images/simbolino1.svg";
+import simbolino2 from "../../assets/images/simbolino2.svg";
 import bar_scuro from "../../assets/images/bar_scuro.svg";
 
 const DrinkDetails = () => {
     let { id } = useParams();
+    const navigate = useNavigate();
     const [drinkDetails, setDrinkDetails] = useState(null);
-
 
     useEffect(() => {
         const fetchDrinkDetails = async () => {
@@ -24,14 +24,29 @@ const DrinkDetails = () => {
         };
 
         fetchDrinkDetails();
-
     }, [id]);
 
+    const findNextDrink = async (currentId, direction) => {
+        let nextId = parseInt(currentId) + direction;
+        while (nextId > 0) {
+            try {
+                const response = await fetch(`https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=${nextId}`);
+                const data = await response.json();
+                if (data.drinks) {
+                    navigate(`/drink/${nextId}`);
+                    return;
+                }
+            } catch (error) {
+                console.error("Errore nell'API:", error);
+            }
+            nextId += direction;
+        }
+    };
+
     if (!drinkDetails) {
-        return <div>Loading...</div>; // Puoi aggiungere un caricamento se i dati non sono ancora disponibili
+        return <div>Loading...</div>;
     }
 
-    // Funzione per ottenere gli ingredienti
     const getIngredients = () => {
         const ingredients = [];
         for (let i = 1; i <= 15; i++) {
@@ -54,16 +69,15 @@ const DrinkDetails = () => {
 
             <div className="align-items-center d-flex flex-column justify-content-center w-100">
                 <div className="align-items-center d-flex justify-content-around mb-4 w-100">
-                    <button className="border-0 bg-transparent">
-                        <img src={simbolino2}/>
+                    <button className="border-0 bg-transparent" onClick={() => findNextDrink(id, -1)}>
+                        <img src={simbolino2} alt="Previous Drink"/>
                     </button>
                     <h1 className={style.drinkTitle}>{drinkDetails.strDrink}</h1>
-                    <button className="border-0 bg-transparent">
-                        <img src={simbolino1}/>
+                    <button className="border-0 bg-transparent" onClick={() => findNextDrink(id, 1)}>
+                        <img src={simbolino1} alt="Next Drink"/>
                     </button>
                 </div>
-                <img src={drinkDetails.strDrinkThumb} alt={drinkDetails.strDrink}
-                     className={`rounded-circle ${style.image}`}/>
+                <img src={drinkDetails.strDrinkThumb} alt={drinkDetails.strDrink} className={`rounded-circle ${style.image}`}/>
             </div>
 
             <div className="mx-5">
@@ -73,8 +87,8 @@ const DrinkDetails = () => {
                         <div key={index}>
                             <div className="align-items-center d-flex justify-content-center mt-3">
                                 {ingredient.ingredientImage &&
-                                    <img src={ingredient.ingredientImage} alt={ingredient.ingredient}
-                                         className={style.imageIngredients}/>}
+                                    <img src={ingredient.ingredientImage} alt={ingredient.ingredient} className={style.imageIngredients}/>
+                                }
                             </div>
                             <p className={`mt-2 text-center ${style.ingredients}`}>
                                 {ingredient.measure ? ingredient.measure : ""} {ingredient.ingredient}
@@ -93,8 +107,6 @@ const DrinkDetails = () => {
                 <h2 className={`mt-5 ${style.subTitle}`}>Glass</h2>
                 <p className={`mt-2 ${style.ingredients}`}>{drinkDetails.strGlass}</p>
             </div>
-
-
         </div>
     );
 };
